3101b4c6edd02b8c0b8eb78d2dc6b9ce9c91278d
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-24 16:57:24 +0300

    add sign function

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import math
 import random
 import string
 import hashlib
+
+
+def gcd(k: int, q: int):
+    if k == 0:
+        return q, 0, 1
+    d, x1, y1 = gcd(q % k, k)
+    x = y1 - (q // k) * x1
+    y = x1
+    return x
+
+
+def generateKey(k, int, q: int):
+    nod, c1, c2 = gcd(k, q)
+    if nod != 1:
+        print('Error')
+    return c1
 
 
 def genKey():
@@ -21,14 +36,14 @@ def genKey():
         g = (x ** R) % p
     print('g = ' + str(g))
     d = random.randint(0, q)
-    e = g**d % p
+    e = g ** d % p
     return p, q, g
 
-def makeSign(p: int, q: int, g: int, d:int, M: string):
+
+def makeSign(p: int, q: int, g: int, d: int, M: string):
     m = int(hashlib.sha256(M.encode()).hexdigest(), 16)
     k = random.randint(1, q)
-    r = (g**k) % p
-
+    r = (g ** k) % p
 
 
 def main():
