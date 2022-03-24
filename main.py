145667bf062606eae0c2aec310e7a8fbf964da74
nikitademedyuk1@gmail.com <nikitademedyuk1@gmail.com>
2022-03-24 16:33:59 +0300

    build generate function

---
+++ main.py
@@ -0,0 +1,38 @@
+import math
+import random
+import string
+import hashlib
+
+
+def genKey():
+    q = 140990220132661942094353836270106675983065715626747592141413983992936990059339
+    print('q = ' + str(q))
+    R = random.randint(0, q // 2) * 2
+    p = q * R + 1
+    print('R = ' + str(R) + '\n')
+    while 2 ** (q * R) % p != 1 or 2 ** R % p == 1:
+        R = random.randint(0, q // 2) * 2
+        p = q * R + 1
+        print('R = ' + str(R) + '\n')
+    x = random.randint(0, p)
+    g = (x ** R) % p
+    while g == 1:
+        x = random.randint(0, p)
+        g = (x ** R) % p
+    print('g = ' + str(g))
+    d = random.randint(0, q)
+    e = g**d % p
+    return p, q, g
+
+def makeSign(p: int, q: int, g: int, d:int, M: string):
+    m = int(hashlib.sha256(M.encode()).hexdigest(), 16)
+    k = random.randint(1, q)
+    r = (g**k) % p
+
+
+
+def main():
+    genKey()
+
+
+main()
